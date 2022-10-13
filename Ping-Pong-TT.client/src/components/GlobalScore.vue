311638ace579bcbbb2962a0c7ead076c7ee08db1
Lumine3449 <>
2022-10-13 11:06:02 -0600

    test push

--- Ping-Pong-TT.client/src/components/GlobalScore.vue
+++ Ping-Pong-TT.client/src/components/GlobalScore.vue
@@ -1,7 +1,6 @@
 <template>
 
   <div class="col-3 m-2">
-
     <ul class="list-group">
       <li class="list-group-item d-flex justify-content-between align-items-center global-board-head">
         <h3 class="text-center text-success">
