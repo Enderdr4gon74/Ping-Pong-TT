78a4de8ac8fa839566d7f05e071523a65be2760c
Lumine3449 <>
2022-10-13 11:21:59 -0600

    div stuff

--- Ping-Pong-TT.client/src/components/GlobalScore.vue
+++ Ping-Pong-TT.client/src/components/GlobalScore.vue
@@ -1,12 +1,13 @@
 <template>
-
   <div class="col-3 m-2">
+
     <ul class="list-group">
       <li class="list-group-item d-flex justify-content-between align-items-center global-board-head">
         <h3 class="text-center text-success">
           GLOBAL LEADERBOARD
         </h3>
       </li>
+
 
 
       <li class="list-group-item d-flex align-items-center global-board text-light">
@@ -16,8 +17,12 @@
             alt="" class="pfp">
           Dugus
         </div>
-        <span class="badge bg-primary rounded-pill justify-content-between">250</span>
+
+        <div>
+          <span class="badge bg-primary rounded-pill justify-content-between">250</span>
+        </div>
       </li>
+
 
 
       <li class="list-group-item d-flex justify-content-between align-items-center global-board text-light">
@@ -27,26 +32,29 @@
             alt="" class="pfp">
           Tristan
         </div>
+
         <div>
           <span class="badge bg-primary rounded-pill">50</span>
         </div>
       </li>
 
 
+
       <li class="list-group-item d-flex justify-content-between align-items-center global-board text-light">
-        <img
-          src="https://st3.depositphotos.com/6672868/13701/v/600/depositphotos_137014128-stock-illustration-user-profile-icon.jpg"
-          alt="" class="pfp">
-        Yo Mama
-        <span class="badge bg-primary rounded-pill">1</span>
+        <div>
+          <img
+            src="https://st3.depositphotos.com/6672868/13701/v/600/depositphotos_137014128-stock-illustration-user-profile-icon.jpg"
+            alt="" class="pfp">
+          Yo Mama
+        </div>
+
+        <div>
+          <span class="badge bg-primary rounded-pill">1</span>
+        </div>
       </li>
     </ul>
 
   </div>
-
-
-
-
 </template>
 
 
