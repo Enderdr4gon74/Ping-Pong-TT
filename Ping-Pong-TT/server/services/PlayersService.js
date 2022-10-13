e1f1699fbc90e6fdf1d6fdd85a71d163454f83f7
Ryan-Thrall <clear>
2022-10-13 09:49:26 -0600

    hello

--- Ping-Pong-TT/server/services/PlayersService.js
+++ Ping-Pong-TT/server/services/PlayersService.js
@@ -5,12 +5,15 @@ import { tourneyService } from "./TourneyService.js"
 class PlayersService {
   async removePlayerUsingTourneyId(tourneyId, user) {
     const tourney = await tourneyService.getTourneyById(tourneyId)
-    if (!tourney.players.find(p=> p.id == user.id)) {
+    if (!tourney.players.find(p => p.id == user.id)) {
       throw new BadRequest("You're not even on that tourney bro! - (pepto bismol)")
     }
-    tourney.players.splice(tourney.players.findIndex(p=> p.id == user.id), 1)
+    tourney.players.splice(tourney.players.findIndex(p => p.id == user.id), 1)
     tourney.save()
-    user = {name: user.name, picture: user.picture, id: user.id}
+
+    tourneyService.generateMatches(tourneyId)
+
+    user = { name: user.name, picture: user.picture, id: user.id }
     return user
   }
 
@@ -19,9 +22,12 @@ class PlayersService {
     if (tourney.players.find(p => p.id == user.id)) {
       throw new BadRequest("You're already on that tourney bro! - (pepto bismol)")
     }
-    user = {name: user.name, picture: user.picture, id: user.id}
+    user = { name: user.name, picture: user.picture, id: user.id }
     tourney.players = [...tourney.players, user]
     tourney.save()
+
+    tourneyService.generateMatches(tourneyId)
+
     return user
     // const tourney2 = await dbContext.Tourneys.findByIdAndUpdate(tourneyId, {$set: {
     //   playerIds: tourney1.playerIds + user
