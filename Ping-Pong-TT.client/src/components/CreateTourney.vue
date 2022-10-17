15830a8cbcaba2396c06015608351746c5847c6a
Samuel Wilkins <samuel.wilkins.official@gmail.com>
2022-10-17 11:33:16 -0600

    Tourney Styling Complete

--- Ping-Pong-TT.client/src/components/CreateTourney.vue
+++ Ping-Pong-TT.client/src/components/CreateTourney.vue
@@ -14,7 +14,7 @@
       <div class="modal-content bg-dark">
         <div class="modal-header">
           <h1 class="modal-title fs-5 text-success" id="exampleModalLabel">Tournament Creation Form</h1>
-          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          <button type="button" class="btn-close bg-danger" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
         <div class="modal-body">
 
