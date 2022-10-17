69867cb647b47f81101729b4cdd940ffd83b41f9
Samuel Wilkins <samuel.wilkins.official@gmail.com>
2022-10-17 11:27:24 -0600

    TourneyFormModalAwesome

--- Ping-Pong-TT.client/src/pages/HomePage.vue
+++ Ping-Pong-TT.client/src/pages/HomePage.vue
@@ -7,9 +7,7 @@
         Find a Tourney
       </button>
     </router-link>
-    <button class="rounded fs-3 btn btn-success">
-      Create a Tourney
-    </button>
+    <CreateTourney />
   </div>
 
 
@@ -83,11 +81,12 @@
 import GlobalScore from "../components/GlobalScore.vue";
 import LoadingScreen from "../components/LoadingScreen.vue";
 import PlayersMorphingCube from "../components/PlayersMorphingCube.vue";
+import CreateTourney from "../components/CreateTourney.vue";
 export default {
   setup() {
     return {};
   },
-  components: { GlobalScore, LoadingScreen, PlayersMorphingCube }
+  components: { GlobalScore, LoadingScreen, PlayersMorphingCube, CreateTourney }
 }
 </script>
 
