5bdd281bea1865af763f691a97426fe5d2b35b6d
Enderdr4gon74 <tristancox03@gmail.com>
2022-11-17 12:44:01 -0700

    cool cool cool cool

--- Ping-Pong-TT.client/src/components/Animations/PIngPongLoader.vue
+++ Ping-Pong-TT.client/src/components/Animations/PIngPongLoader.vue
@@ -27,7 +27,7 @@
       <div class="ball-container">
         <svg class="ball">
           <circle cx="20" cy="20" r="12" />
-          <!-- <image height="50"
+          <!-- <image height="100" width="100"
             href="https://images.ctfassets.net/dqh2h7mvo1xq/7JsbDikYLbhDgPuTdqGhz3/4600c4ff48cfe6a32987244a48826269/00301490039564_C1N1.png" /> -->
         </svg>
       </div>
